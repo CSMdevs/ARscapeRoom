b0d35701a1e430680b2100bc7f714b0a2c323019
Cemre Suler <Cemre8@live.nl>
2018-04-02 20:07:15 +0200

    Fixed Minor issue with homescreen

---
+++ Assets/Scripts/FirstTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class FirstTime : MonoBehaviour {
+	public string SceneToLoad;
+	public Canvas test;
+	// Use this for initialization
+	void Start () {
+		test.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Joepie() {
+		test.enabled = true;
+	}
+	public void TapToStart (){
+		SceneManager.LoadScene (SceneToLoad);
+		Debug.Log("Start");
+	}
+}
