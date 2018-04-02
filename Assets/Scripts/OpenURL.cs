5eeb7efe810942936aea5b45a2f14671b99d9793
Cemre Suler <Cemre8@live.nl>
2018-04-02 18:46:02 +0200

    Added print button

---
+++ Assets/Scripts/OpenURL.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Url() {
+		Application.OpenURL("https://github.com/CSMdevs/ARscapeRoom/raw/master/Slot.JPG");
+	}
+}
